2d425143e705c7d3be2e0be9c7a99babca4b0718
Adam Fenemore <AdamFe@intergen.org.nz>
2018-10-16 13:56:35 +1300

    Second commit. basics up and running, no css yet

--- DogwatchEpi/DogwatchEpi/Models/Pages/LandingPage.cs
+++ DogwatchEpi/DogwatchEpi/Models/Pages/LandingPage.cs
@@ -10,7 +10,7 @@ namespace DogwatchEpi.Models.Pages
     [ContentType(DisplayName = "LandingPage", GUID = "8aaa6434-a5e0-4222-a037-02ac98e5c404", Description = "")]
     public class LandingPage : PageData
     {
-        /*
+        
                 [CultureSpecific]
                 [Display(
                     Name = "Main body",
@@ -18,6 +18,6 @@ namespace DogwatchEpi.Models.Pages
                     GroupName = SystemTabNames.Content,
                     Order = 1)]
                 public virtual XhtmlString MainBody { get; set; }
-         */
+         
     }
 }
---
+++ DogwatchEpi/DogwatchEpi/Models/Pages/StandardPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace DogwatchEpi.Models.Pages
+{
+    [ContentType(DisplayName = "StandardPage", GUID = "2f669c4b-f678-4d59-bbd9-42fd4e88c1a1", Description = "")]
+    public class StandardPage : PageData
+    {
+
+
+        [CultureSpecific]
+        [Display(
+            Name = "Heading",
+            Description = "The page heading",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual String Heading { get; set; }
+
+
+        [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+
+
+
+        [Display(
+            GroupName = SystemTabNames.Settings,
+            Order = 200)]
+        [CultureSpecific]
+        public virtual bool HideSiteHeader { get; set; }
+
+        [Display(
+            GroupName = SystemTabNames.Settings,
+            Order = 300)]
+        [CultureSpecific]
+        public virtual bool HideSiteFooter { get; set; }
+
+    }
+}
